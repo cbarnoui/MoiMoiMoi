4b71c5dc48ca4d272f3dd926f81414f064e4b18b
cb9592 <cb9592@gmail.com>
2026-05-10 22:40:59 +0200

    Fix size

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\cb959\AppData\Local\Temp\tmprzfhs01x.js
+// include: C:\Users\cb959\AppData\Local\Temp\tmprnqzrlxa.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -211,21 +211,21 @@ Module['FS_createPath']("/assets", "Sounds", true, true);
 
   })();
 
-// end include: C:\Users\cb959\AppData\Local\Temp\tmprzfhs01x.js
-// include: C:\Users\cb959\AppData\Local\Temp\tmptwt9s7_3.js
+// end include: C:\Users\cb959\AppData\Local\Temp\tmprnqzrlxa.js
+// include: C:\Users\cb959\AppData\Local\Temp\tmp1dkah9l8.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\cb959\AppData\Local\Temp\tmptwt9s7_3.js
-// include: C:\Users\cb959\AppData\Local\Temp\tmpxkhee3jl.js
+  // end include: C:\Users\cb959\AppData\Local\Temp\tmp1dkah9l8.js
+// include: C:\Users\cb959\AppData\Local\Temp\tmp_5gf5ptt.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\cb959\AppData\Local\Temp\tmpxkhee3jl.js
+  // end include: C:\Users\cb959\AppData\Local\Temp\tmp_5gf5ptt.js
 
 
 var arguments_ = [];
